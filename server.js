e33d3ac9d18724adfa4a7e2eeaa9cf2f6d0f2fc1
Tavius Koktavy <tavius@nyu.edu>
2022-08-22 04:33:19 -0700

    Get index?

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ const options = {
 const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
 // const server = app.listen(PORT, () => {
 //   console.log(`Server started on port ${PORT}`);
 // });
